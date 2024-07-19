708f482020cc1f9a3514be9f5ca42cfe0ffcf6c5
URGINSAN <32620438+URGINSAN@users.noreply.github.com>
2024-07-19 22:54:32 +0300

    up

---
+++ Assets/Content/Scripts/Player/PlayerTankMovement.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class PlayerTankMovement : MonoBehaviour
+{
+    private float playerInputX = 0;
+    private float playerInputY = 0;
+
+    [SerializeField] private float speed = 250;
+    private Rigidbody2D rb;
+    [SerializeField] Transform TankTracks;
+    private float tracksRotZ;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        playerInputX = Input.GetAxisRaw("Horizontal");
+        playerInputY = Input.GetAxisRaw("Vertical");
+        //SwapSprite();
+    }
+
+    void SwapSprite()
+    {
+        if (playerInputX > 0)// Right
+        {
+            transform.localScale = new Vector3(
+                Mathf.Abs(transform.localScale.x),
+                transform.localScale.y,
+                transform.localScale.z
+            );
+        }
+        else if (playerInputX < 0)// Left
+        {
+            transform.localScale = new Vector3(
+                -1 * Mathf.Abs(transform.localScale.x),
+                transform.localScale.y,
+                transform.localScale.z
+            );
+        }
+
+        if (playerInputY > 0)// Up
+        {
+            transform.localScale = new Vector3(
+                Mathf.Abs(transform.localScale.x),
+                transform.localScale.y,
+                transform.localScale.z
+            );
+        }
+        else if (playerInputY < 0)// Down
+        {
+            transform.localScale = new Vector3(
+                -1 * Mathf.Abs(transform.localScale.x),
+                transform.localScale.y,
+                transform.localScale.z
+            );
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(
+            playerInputX * speed * Time.fixedDeltaTime,
+            playerInputY * speed * Time.fixedDeltaTime
+        );
+
+        tracksRotZ = Mathf.MoveTowards(tracksRotZ, -playerInputX * 35, 5);
+        tracksRotZ = Mathf.Clamp(tracksRotZ, -35, 35);
+        TankTracks.localEulerAngles = new Vector3(0, 0, tracksRotZ);
+    }
+}
